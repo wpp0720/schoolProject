813ffaa3f32183a5a75bec00f530871386d84701
wpp0720 <wpp0720@126.com>
2018-09-02 23:40:29 +0800

    9.2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
+import topTeacher from '@/components/topTeacher'
+import school from '@/components/school'
+import contactUS from '@/components/contactUS'
+import teacherTeam from '@/components/teacherTeam'
+import curriculaVariable from '@/components/curriculaVariable'
 
 
 Vue.use(Router)
@@ -17,6 +22,31 @@ export default new Router({
       path: '/index',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/topTeacher',
+      name: 'topTeacher',
+      component: topTeacher
+    },
+    {
+      path: '/school',
+      name: 'school',
+      component: school
+    },
+    {
+      path: '/contactUS',
+      name: 'contactUS',
+      component: contactUS
+    },
+    {
+      path: '/teacherTeam',
+      name: 'teacherTeam',
+      component: teacherTeam
+    },
+    {
+      path: '/curriculaVariable',
+      name: 'curriculaVariable',
+      component: curriculaVariable
     }
   ]
 });
